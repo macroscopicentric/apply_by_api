83246382f3e87a2dd44aa67e898da5ad091b7ad4
Rachel King <rachel.b.king@gmail.com>
2014-09-04 16:28:38 -0400

    Added header, wrapped in a function.

--- apply.py
+++ apply.py
@@ -1,7 +1,15 @@
 import requests
-from resume import resume_pdf, application_dict
+import json
+from data import resume_pdf, application_dict, url
 
-with open(resume_pdf) as resume:
-    encoded_resume = resume.read().encode('base64')
+def send_resume():
+    with open(resume_pdf) as resume:
+        encoded_resume = resume.read().encode('base64')
+    application_dict['resume'] = encoded_resume
 
-application_dict['resume'] = encoded_resume
+    header = {'content-type': 'application/json'}
+
+    requests.post(url, data=json.dumps(application_dict), headers=header)
+
+if '__name__' == '__main__':
+    send_resume()
