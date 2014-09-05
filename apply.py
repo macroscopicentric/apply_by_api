b0ad6a30f9d171ac6b7d642cdbefdce9a0e00eb6
Rachel King <rachel.b.king@gmail.com>
2014-09-05 13:10:41 -0400

    Added sanity check function to test formatting.

--- apply.py
+++ apply.py
@@ -5,8 +5,13 @@ from data import resume_pdf, application_dict, url
 def format_json():
     with open(resume_pdf) as resume:
         encoded_resume = resume.read().encode('base64')
+    print encoded_resume
     application_dict['resume'] = encoded_resume
+    # print application_dict
 
+def sanity_check(encoded_resume):
+    with open('resume_test.pdf', 'w') as resurrected_resume:
+        resurrected_resume.write(encoded_resume.decode('base64'))
 
 def send_resume():
     header = {'content-type': 'application/json'}
@@ -14,3 +19,4 @@ def send_resume():
 
 if __name__ == '__main__':
     format_json()
+    sanity_check(application_dict['resume'])
