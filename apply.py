e79c0853b044832bb73b394ff54b3eed60692424
Rachel King <rachel.b.king@gmail.com>
2014-09-05 13:09:56 -0400

    Added modularity.

--- apply.py
+++ apply.py
@@ -2,14 +2,15 @@ import requests
 import json
 from data import resume_pdf, application_dict, url
 
-def send_resume():
+def format_json():
     with open(resume_pdf) as resume:
         encoded_resume = resume.read().encode('base64')
     application_dict['resume'] = encoded_resume
 
-    header = {'content-type': 'application/json'}
 
+def send_resume():
+    header = {'content-type': 'application/json'}
     requests.post(url, data=json.dumps(application_dict), headers=header)
 
-if '__name__' == '__main__':
-    send_resume()
+if __name__ == '__main__':
+    format_json()
